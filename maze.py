efbbbe126dd1f5943f38c1b771d54df8bb69e571
Mohamad <mohammad_kozeiha@hotmail.com>
2021-07-03 17:21:57 +0200

    add the magic_maze code

--- maze.py
+++ maze.py
@@ -1,27 +1,34 @@
+from PIL import Image
 from random import shuffle, randrange
+
+
+def generate_maze(width, height):
+    visited = [[0] * width + [1] for _ in range(height)] + [[1] * (width + 1)]
+    vertical = [["#.."] * width + ['#'] for _ in range(height)] + [[]]
+    horizental = [["###"] * width + ['#'] for _ in range(height + 1)]
  
-def make_maze(w = 16, h = 8):
-    vis = [[0] * w + [1] for _ in range(h)] + [[1] * (w + 1)]
-    ver = [[".#.."] * w + ['#'] for _ in range(h)] + [[]]
-    hor = [["###"] * w + ['#'] for _ in range(h + 1)]
- 
-    def walk(x, y):
-        vis[y][x] = 1
+    def generate(x, y):
+        visited[y][x] = 1
  
         d = [(x - 1, y), (x, y + 1), (x + 1, y), (x, y - 1)]
         shuffle(d)
-        for (xx, yy) in d:
-            if vis[yy][xx]: continue
-            if xx == x: hor[max(y, yy)][x] = "#.."
-            if yy == y: ver[y][max(x, xx)] = "..."
-            walk(xx, yy)
+        for (ii, jj) in d:
+            if visited[jj][ii]:
+                continue
+            if ii == x:
+                horizental[max(y, jj)][x] = "#.."
+            if jj == y:
+                vertical[y][max(x, ii)] = "..."
+            generate(ii, jj)
  
-    walk(randrange(w), randrange(h))
+    generate(randrange(width), randrange(height))
  
     s = ""
-    for (a, b) in zip(hor, ver):
+    for (a, b) in zip(horizental, vertical):
         s += ''.join(a + ['\n'] + b + ['\n'])
     return s
+
  
 if __name__ == '__main__':
-    print(make_maze())
+    print(generate_maze(40,40))
+    
