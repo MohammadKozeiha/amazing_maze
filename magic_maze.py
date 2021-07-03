9046e17668b6b71743d540e9e8606d41b2c9534f
Mohamad <mohammad_kozeiha@hotmail.com>
2021-07-03 18:15:03 +0200

    final tests

--- magic_maze.py
+++ magic_maze.py
@@ -1,18 +1,11 @@
-
-###################################
-# Python maze generator program
-# using PyGame for animation
-# Davis MT
-# Python 3.4
-# 10.02.2018
-###################################
-
 import pygame
 import time
 import random
+file = 'file_example_WAV_1MG.wav'
 
 # set up pygame window
-n=10
+print('please enter the maze width : ')
+n=int(input())
 
 WIDTH =n*n +100
 HEIGHT = n*n + 100
@@ -20,13 +13,15 @@ FPS = 150
 
 # Define colours
 WHITE = (255, 255, 255)
-GREEN = (0, 255, 0,)
-BLUE = (0, 0, 255)
-YELLOW = (255 ,255 ,0)
+RED = (255, 0, 0,)
+BROWN = (150, 75, 0)
+ORANGE = (255 ,165 ,0)
 
 # initalise Pygame
 pygame.init()
 pygame.mixer.init()
+#pygame.mixer.music.load(file)
+#pygame.mixer.music.play(-1) # If the loops is -1 then the music will repeat indefinitely.
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Maze Generator")
 clock = pygame.time.Clock()
@@ -55,37 +50,37 @@ def build_grid(x, y, w):
 
 
 def push_up(x, y):
-    pygame.draw.rect(screen, BLUE, (x + 1, y - w + 1, w-1, 2*w-1), 0)         # draw a rectangle twice the width of the cell
+    pygame.draw.rect(screen, BROWN, (x + 1, y - w + 1, w-1, 2*w-1), 0)         # draw a rectangle twice the width of the cell
     pygame.display.update()                                              # to animate the wall being removed
 
 
 def push_down(x, y):
-    pygame.draw.rect(screen, BLUE, (x +  1, y + 1, w-1, 2*w-1), 0)
+    pygame.draw.rect(screen, BROWN, (x +  1, y + 1, w-1, 2*w-1), 0)
     pygame.display.update()
 
 
 def push_left(x, y):
-    pygame.draw.rect(screen, BLUE, (x - w +1, y +1, 2*w-1, w-1), 0)
+    pygame.draw.rect(screen, BROWN, (x - w +1, y +1, 2*w-1, w-1), 0)
     pygame.display.update()
 
 
 def push_right(x, y):
-    pygame.draw.rect(screen, BLUE, (x +1, y +1, 2*w-1, w-1), 0)
+    pygame.draw.rect(screen, BROWN, (x +1, y +1, 2*w-1, w-1), 0)
     pygame.display.update()
 
 
 def single_cell( x, y):
-    pygame.draw.rect(screen, GREEN, (x +1, y +1, w-2, w-2), 0)          # draw a single width cell
+    pygame.draw.rect(screen, RED, (x +1, y +1, w-2, w-2), 0)          # draw a single width cell
     pygame.display.update()
 
 
 def backtracking_cell(x, y):
-    pygame.draw.rect(screen, BLUE, (x +1, y +1, w-2, w-2), 0)        # used to re-colour the path after single_cell
+    pygame.draw.rect(screen, BROWN, (x +1, y +1, w-2, w-2), 0)        # used to re-colour the path after single_cell
     pygame.display.update()                                        # has visited cell
 
 
 def solution_cell(x,y):
-    pygame.draw.rect(screen, YELLOW, (x+3, y+3, 3, 3), 0)             # used to show the solution
+    pygame.draw.rect(screen, ORANGE, (x+3, y+3, 3, 3), 0)             # used to show the solution
     pygame.display.update()                                        # has visited cell
 
 
@@ -170,4 +165,4 @@ while running:
         # check for closing the window
         if event.type == pygame.QUIT:
             running = False
-import pdb; pdb.set_trace()
+#import pdb; pdb.set_trace()
